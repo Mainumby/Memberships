ccaf149ef3f7b6492ad0dd7cfa179bdce81ccf61
Dario Ramirez <dari_din@hotmail.com>
2018-03-01 20:26:37 -0300

    Nuevos cambios

---
+++ Memberships/Entities/UserSubscription.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace Memberships.Entities
+{
+    [Table("UserSubscription")]
+    public class UserSubscription
+    {
+        //Composite key: Subscription and AspNetUser Table
+        [Key,Column(Order =1)]
+        [Required]
+        public int SubscriptionId { get; set; }
+        [Key,Column(Order =2)]
+        [MaxLength(128)]
+        [Required]
+        public string UserId { get; set; }
+        public DateTime? StarDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
